38f55bcdf339caffaf1916fec7c23af5935fb5b4
Kayson <kayson.fakhar@gmail.com>
2021-08-30 18:28:06 +0200

    re-organized functions, added a couple more. made I/O more consistent, I hope.

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 import copy
-from typing import Callable, Dict, List
+from typing import Callable, Dict, List, Optional
+import matplotlib.pyplot as plt
 
 
 def make_sin(*,
@@ -10,60 +11,123 @@ def make_sin(*,
     """
     Create a sin wave with the given parameters such that there will be "f" peaks during "t" timepoints.
     So if f is weirdly large (like more than 1 peak per "t") then the signal won't be sinusoidal anymore.
-    It will be trash. Same for weirdly small f.
-
+    Same for weirdly small f.
     Args:
-        f (int): 
-            Frequency of the sine wave. 
+        f (int):
+            Frequency of the sine wave.
             For now stick to a range of [1, t/10]
-            
-        t (int): 
-            Number of time points.
 
+        t (int):
+            Number of time points.
     Returns (np.ndarray):
-        A sin wave.
-    
-    TODO: Error handling and exceptions. 
+        A sinusoidal time series with length t.
+
+    TODO: Error handling and exceptions.
     TODO: Check for the f/t ratio.
-     
+
     """
     return np.sin(2 * np.pi * f * np.arange(t) / t)
 
 
-def reverse_in_time(signal: List) -> List:
+def make_retrograde(*,
+                    stimulus: np.ndarray) -> np.ndarray:
     """
-    Reverse the time axis of a signal.
-    
-    """
-    return signal[-1::-1]
+    Reverse the temporal order of the reference stimulus and makes a "match" condition to be used as the target.
 
-
-def make_one_event_off(signal: List,
-                       n_events: int,
-                       new_f: int) -> np.ndarray:
-    """
-    Randomly chooses an event and changes its frequency to a new one using the make_sin function.
     Args:
-        signal (List):
-            The original signal, just the first stimulus part.
-
-        n_events (int):
-            Self explanatory.
-
-        new_f (int):
-            This too!
+        stimulus (List): the reference stimulus.
 
     Returns (np.ndarray):
-        The second stimulus in the no_match conditions.
+        Mirrored stimulus to be placed as the target.
 
+    """
+    return np.array(stimulus[::-1])
+
+
+def make_sin_stimulus(*,
+                      frequencies: List,
+                      event_duration: int = 500) -> np.ndarray:
+    """
+    Generates a vector of sinusoidal events given the frequencies by looping through them.
+    So the order of events will be the same as the elements in the list.
+    Note: it uses the "make_sin" function.
+
+    Args:
+        frequencies (List):
+            Just as "make_sin", keep the duration in mind. If the ratio is off (like more than one peak per sample)
+            then the signal will be trash instead of sinusoidal! So max(frequency) < event_duration/10
+
+        event_duration (int):
+            Number of samples per frequency.
+            So the time series will have (len(frequencies) * event_duration) samples.
+
+    Returns (np.ndarray):
+        The first stimulus (reference stimulus) that can be then transformed
+        or directly recalled as the target stimulus.
+
+    TODO: enforce the frequency/duration ratio
+    """
+    stimulus = []
+    for frequency in frequencies:
+        stimulus.extend(make_sin(f=frequency,
+                                 t=event_duration))
+    return np.array(stimulus)
+
+
+def make_one_event_off(*,
+                       stimulus: np.ndarray,
+                       n_events: int,
+                       new_f: int,
+                       is_retrograde: bool = False) -> np.ndarray:
+    """
+    Randomly chooses an event and changes its frequency to the given new frequency using the "make_sin" function.
+    Makes a "no_match" condition of the reference stimulus,
+    either in a recall condition or a retrograde working memory condition.
+
+    Args:
+
+        stimulus (List):
+            The reference stimulus.
+
+        n_events (int):
+            Assumes the stimulus has events with equal lengths.
+
+        new_f (int):
+            A random event will be replaced with a new sinusoidal event with this frequency.
+
+        is_retrograde (bool):
+           If True, it mirrors the reference signal first (using make_retrograde) and then makes an event off.
+    Returns (np.ndarray):
+        The second stimulus (target stimulus) in the no_match conditions.
+
+    TODO: The new frequency shouldn't be the same as the one it's replacing. Handle that.
     TODO: What about N events_off?
     """
-    altered_signal = copy.deepcopy(signal)
-    event_length = int(len(altered_signal) / n_events)
+    target_stimulus = copy.deepcopy(stimulus)
+    if is_retrograde:
+        target_stimulus = make_retrograde(stimulus=target_stimulus)
+    event_length = int(len(target_stimulus) / n_events)
     event_index = np.random.choice(n_events)
-    altered_signal[(event_index * event_length):(event_index * event_length) + event_length] = make_sin(f=new_f,
-                                                                                                        t=event_length)
-    return np.array(altered_signal)
+    target_stimulus[(event_index * event_length):(event_index * event_length) + event_length] = make_sin(f=new_f,
+                                                                                                         t=event_length)
+    return np.array(target_stimulus)
+
+
+def make_identical(*,
+                   stimulus: np.ndarray) -> np.ndarray:
+    """
+        This might look unnecessary but it can be fed into the trial generator as the "transformation" function.
+        It's just to make things more explicit.
+        Naturally, makes a "Match" condition of the reference.
+    Args:
+        stimulus:
+            The reference stimulus, of course.
+
+    Returns:
+        Copy of the reference for the recall condition.
+
+    """
+    return stimulus
 
 
 def make_cue_signal(*,
@@ -71,12 +135,12 @@ def make_cue_signal(*,
                     cue_onset: int,
                     cue_offset: int):
     """
-    Generates the cue signal in which the agent should replicate during the task.
-    It is a zero vector a step function at one point showing the "go" signal.
+    Generates the cue signal in which the ANN should replicate during the task.
+    It is a zero vector a step function showing the "go" signal.
 
     Args:
         t (int): Duration of the whole signal. Should match the trial duration.
-        cue_onset (int): When to start the cue. Should match the end of transformed stimulus.
+        cue_onset (int): When to start the cue. Should match the end of the target stimulus.
         cue_offset (int): When to stop the cue. Should match the end of the trial.
 
     Returns (np.ndarray): cue signal for one trial
@@ -86,31 +150,25 @@ def make_cue_signal(*,
     return np.hstack((no_go, go))
 
 
-def make_stimulus(*,
-                  function: Callable = make_sin,
-                  frequencies: List = None,
-                  silence_duration: int = 1_000,
-                  global_noise: float = .0,
-                  response_duration: int = 500,
-                  transformation: Callable = None,
-                  cue_generator: Callable = make_cue_signal,
-                  function_params: Dict,
-                  transformation_params: Dict) -> np.ndarray:
+def make_stimuli_trial(*,
+                       stimulus: np.ndarray,
+                       silence_duration: int = 1_000,
+                       global_noise: float = .0,
+                       response_duration: int = 500,
+                       transformation: Callable = None,
+                       cue_generator: Callable = make_cue_signal,
+                       transformation_params: Optional[Dict]) -> np.ndarray:
     """
-    Create one trial of the working memory task stimulus starting from 
-    the stimulus generated by the function, followed by the silence,
-    followed by the transformed (or not) stimulus, and ends with the
-    response period.
+    Create stimuli of one trial for the working memory/recall task by stacking
+    reference stimulus, silence, target stimulus (transformed or not), response duration.
 
     Args:
-        function (Callable): 
-            Function to generate the stimulus.
-            
-        frequencies (List): 
-            List of frequencies to generate events with so len(frequencies) == n_events.
-            
+        stimulus (np.ndarray):
+            The reference stimulus that is presented in the first part of the trial (pre delay).
+            It will be passed to "transformation" if needed (for the working memory condition).
+
         silence_duration (int): 
-            self explanatory! the longer the silence the harder.
+            self explanatory! the longer the silence the harder, I guess!
         
         global_noise (float):
             adds noise to the whole signal (not the cue) so should make the task harder.
@@ -118,48 +176,42 @@ def make_stimulus(*,
             Drawn from a Gaussian distribution with mean 0.
             
         response_duration (int): 
-            Duration of the response.
+            Duration of the response that the ANN should hold onto the correct answer.
             
         transformation (Callable): 
-            Transformation to apply to the stimulus.
-            Based on its own parameters (see below)
+            Transformation to apply to the reference stimulus to make the target stimulus.
+            Based on its own parameters (see below).
 
         cue_generator (Callable): 
             Function to generate the cue signal. 
             Should get a signal and return a signal.
-            
-        function_params (Dict) : 
-            Parameters to pass to the event generator. 
-            There should be a pair for the frequency called "f"
 
-        transformation_params (Dict) :
+        transformation_params Optional[Dict] :
             Parameters to pass to the transformation function (if it needs anything more than the signal itself).
 
     Returns (np.ndarray):
-        One trial of the working memory task (signal, cue).
+        One trial of the working memory/recall task (signal, cue).
+        It returns both signals in a 2D np array. so [0] is the signal [1] is the cue.
     
     TODO: Error handling and exceptions.
-        
+    TODO: Enforce the internal I/O formatting.
+    TODO: a parameter called stimulus is passed to the transformation. Another flexible solution?
     """
-    # Generate the stimulus.
-    stimulus = []
-
-    for frequency in frequencies:
-        function_params["f"] = frequency
-        stimulus.extend(function(**function_params))
-
+    transformation_params = transformation_params if transformation_params else {}
     # Generate the second part of the stimulus (transform if needed).
     if transformation is not None:
-        stimulus_transformed = transformation(stimulus,**transformation_params)
+        stimulus_transformed = transformation(stimulus=stimulus,
+                                              **transformation_params)
     else:
         stimulus_transformed = stimulus
 
     # Generate the silence.
     silence = np.zeros(silence_duration)
 
-    signal = np.hstack((np.array(stimulus),
+    # adding everything up (stimulus, silence, transformed (or not) stimulus, response time)
+    signal = np.hstack((stimulus,
                         silence,
-                        np.array(stimulus_transformed),
+                        stimulus_transformed,
                         np.zeros(response_duration)))
     trial_duration = len(signal)
 
@@ -169,13 +221,28 @@ def make_stimulus(*,
     cue_on = trial_duration - response_duration
     cue_signal = cue_generator(t=trial_duration, cue_onset=cue_on, cue_offset=trial_duration)
 
-    # adding everything up (stimulus, silence, transformed stimulus, response time)
-
     return np.array((signal, cue_signal))
 
-# TODO: Frequency modulation
+
 # TODO: What is a good output format? numpy or pandas?
 # TODO: Refactor to make it more readable.
 
-
-##============================================================================================##
+# frequency_matrix = np.random.randint(low=5, high=30, size=(3, 4))
+# modulated_frequencies = np.random.randint(low=1, high=3, size=(3, 1))
+# stim = []
+# trial = []
+# for indx, frequencies in enumerate(frequency_matrix):
+#     make_one_event_off_params = {"n_events": int(len(frequencies)),
+#                                  "new_f": int(modulated_frequencies[indx]),
+#                                  "is_retrograde": True}
+#     stim.append(make_sin_stimulus(frequencies=frequencies, event_duration=500))
+#     trial.append(
+#         make_stimuli_trial(stimulus=np.array(stim[indx]),
+#                            silence_duration=1000,
+#                            global_noise=.0,
+#                            response_duration=500,
+#                            transformation=make_one_event_off,
+#                            cue_generator=make_cue_signal,
+#                            transformation_params=make_one_event_off_params))
+# plt.plot(trial[0][0]);
+# plt.show()
